2f1db985640de19d5f8224e54c77c8daaebe4a33
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-10 13:47:02 +0200

    Roughly works

---
+++ contentScript.js
@@ -0,0 +1,152 @@
+let globalTweetMap = new Map();
+
+function extractTweetId(tweet) {
+    const linkElement = tweet.querySelector('a[href*="/status/"]');
+    let tweetId = null;
+    if (linkElement) {
+        const href = linkElement.getAttribute('href');
+        const statusPosition = href.indexOf('/status/') + 8; // +8 to move past '/status/'
+        tweetId = href.substring(statusPosition);
+    }
+    return tweetId;
+}
+
+function extractLikes(tweet) {
+    const likeButton = tweet.querySelector('button[data-testid="like"]');
+    let likesCount = 0;
+    if (likeButton) {
+        const likesText = likeButton.querySelector('div > div > span > span').innerText;
+        likesCount = parseInt(likesText, 10);
+    }
+    return likesCount;
+}
+
+function getContent(textsDom) {
+    let content = "";
+    for (const textDom of textsDom) {
+        if (textDom.tagName.toLowerCase() === "span") {
+            content += textDom.innerText;
+        }
+        if (textDom.tagName.toLowerCase() === "img") {
+            content += textDom.getAttribute("alt");
+        }
+    }
+    return content;
+}
+
+function extractTweetPhotoUrl(tweet) {
+    const photoDiv = tweet.querySelector('[data-testid="tweetPhoto"]');
+    let photoUrl = null;
+    if (photoDiv) {
+        const imgTag = photoDiv.querySelector('img');
+        if (imgTag) {
+            photoUrl = imgTag.getAttribute('src');
+        }
+    }
+    return photoUrl;
+}
+
+function extractTweetDate(tweet) {
+    const timeElement = tweet.querySelector('time');
+    let tweetDate = null;
+    if (timeElement) {
+        tweetDate = timeElement.getAttribute('datetime'); 
+    }
+    return tweetDate;
+}
+
+function extractCommentCount(tweet) {
+    const replyButton = tweet.querySelector('button[data-testid="reply"]');
+    let commentCount = 0;
+    if (replyButton) {
+        const commentTextElement = replyButton.querySelector('div > div > span > span > span');
+        if (commentTextElement && commentTextElement.innerText) {
+            commentCount = parseInt(commentTextElement.innerText, 10);
+        }
+    }
+    return commentCount;
+}
+
+function extractRetweetCount(tweet) {
+    const retweetButton = tweet.querySelector('button[data-testid="retweet"]');
+    let retweetCount = 0;
+    if (retweetButton) {
+        const retweetTextContainer = retweetButton.querySelector('div > div > span > span');
+        if (retweetTextContainer && retweetTextContainer.innerText) {
+            const retweetText = retweetTextContainer.innerText;
+            retweetCount = parseInt(retweetText, 10) || 0;
+        }
+    }
+    return retweetCount;
+}
+
+function extractViewCount(tweet) {
+    const viewLink = tweet.querySelector('a[aria-label*="views"]');
+    let viewCount = 0;
+    if (viewLink) {
+        const viewTextElement = viewLink.querySelector('div > div > span > span > span');
+        if (viewTextElement && viewTextElement.innerText) {
+            const viewText = viewTextElement.innerText;
+            // Convert '19K' to 19000 and '1.2M' to 1200000, etc.
+            if (viewText.endsWith('K')) {
+                viewCount = parseFloat(viewText) * 1000;
+            } else if (viewText.endsWith('M')) {
+                viewCount = parseFloat(viewText) * 1000000;
+            } else {
+                viewCount = parseInt(viewText, 10);
+            }
+        }
+    }
+    return viewCount;
+}
+
+function userLikesTweet(tweet) {
+    const likeButton = tweet.querySelector('button[data-testid="like"]');
+    if (likeButton) {
+        return false;
+    }
+    return true; 
+}
+
+function populateGlobalTweetMap() {
+    const tweets = document.querySelectorAll("article");
+    for (const tweet of tweets) {
+        const textsDom = tweet.querySelectorAll("[data-testid=tweetText] > *");
+
+        let tweetId = extractTweetId(tweet);
+        let likes = extractLikes(tweet);
+        let content = getContent(textsDom);
+        let photo = extractTweetPhotoUrl(tweet);
+        let date = extractTweetDate(tweet);
+        let comments = extractCommentCount(tweet);
+        let retweets = extractRetweetCount(tweet);
+        let views = extractViewCount(tweet);
+        let userLikesTheTweet = userLikesTweet(tweet);
+
+        if (tweetId && content) {
+            let tweetData = new Map();
+            tweetData.set("content", content);
+            if (photo) {
+                tweetData.set("photo", photo);
+            }
+            tweetData.set("likes", likes);
+            tweetData.set("date", date);
+            tweetData.set("comments", comments);
+            tweetData.set("retweets", retweets);
+            tweetData.set("views", views);
+            tweetData.set("userLikesTweet", userLikesTheTweet);
+
+            globalTweetMap.set(tweetId, tweetData);
+            console.log(globalTweetMap);
+        }
+    }
+}
+
+function parseState() {
+    populateGlobalTweetMap();
+}
+
+console.log("Running Kinode Extension"); 
+parseState();
+setInterval(parseState, 5000); 
+
---
+++ popup.js
@@ -0,0 +1,142 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchSettings();
+    document.getElementById('add-rule').addEventListener('click', function() {
+        addRule();
+    });
+    document.getElementById('remove-rule').addEventListener('click', function() {
+        removeRule();
+    });
+    document.getElementById('port').addEventListener('change', function() {
+        const portValue = this.value;
+        chrome.storage.local.set({'port': portValue}, function() {
+            console.log('Port value saved:', portValue);
+        });
+    });
+    document.getElementById('api-key').addEventListener('change', function() {
+        const apiKeyValue = this.value;
+        chrome.storage.local.set({'api_key': apiKeyValue}, function() {
+            console.log('API Key value saved:', apiKeyValue);
+        });
+    });
+    document.getElementById('save').addEventListener('click', submitSettings);
+
+    document.getElementById('toggle').addEventListener('click', showCheckmark);
+    document.getElementById('checkmark').addEventListener('click', toggleCheckmark);
+});
+
+function fetchSettings() {
+    chrome.storage.local.get(['port', 'api_key'], function(result) {
+        const port = result.port || '8080';
+        document.getElementById('port').value = port;
+
+        const apiKey = result.api_key || '';
+        document.getElementById('api-key').value = apiKey;
+
+        fetch(`http://localhost:${port}/main:filter:appattacc.os/fetch_settings`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
+        })
+        .then(response => response.json())
+        .then(data => {
+            displayRules(data.rules);
+            setToggleState(data.is_on);
+        })
+        .catch(error => console.error('Error fetching settings:', error));
+    });
+}
+
+function displayRules(rules) {
+    const container = document.getElementById('rules-container');
+    container.innerHTML = '';
+    rules.forEach(rule => {
+        const textareaElement = document.createElement('textarea');
+        textareaElement.classList.add('rule');
+        textareaElement.value = rule;
+        textareaElement.addEventListener('input', autoResize, false);
+        container.appendChild(textareaElement);
+    });
+}
+
+function setToggleState(is_on) {
+    const toggle = document.getElementById('toggle');
+    toggle.checked = is_on;
+}
+
+function addRule() {
+    const container = document.getElementById('rules-container');
+    const textareaElement = document.createElement('textarea');
+    textareaElement.classList.add('rule');
+    textareaElement.placeholder = "Enter a rule";
+    textareaElement.addEventListener('input', autoResize, false);
+    container.appendChild(textareaElement);
+}
+
+function removeRule() {
+    const container = document.getElementById('rules-container');
+    const lastRuleElement = container.lastElementChild;
+    if (lastRuleElement) {
+        container.removeChild(lastRuleElement);
+    }
+}
+
+let timeoutId;
+
+function submitSettings() {
+    const port = document.getElementById('port').value || '8080';
+    const rules = Array.from(document.querySelectorAll('.rule')).map(input => input.value);
+    const is_on = document.getElementById('toggle').checked;
+    const api_key = document.getElementById('api-key').value;
+
+    fetch(`http://localhost:${port}/main:filter:appattacc.os/submit_settings`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            rules: rules,
+            is_on: is_on,
+            api_key: api_key
+        })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        fetchSettings();
+    }).catch(error => console.error('Error submitting settings:', error));
+
+}
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+}
+
+function toggleCheckmark(e) {
+    if (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+    var checkmark = document.getElementById("checkmark");
+    var toggle = document.getElementById("toggle");
+    if (toggle.checked) {
+        checkmark.style.display = "none";
+        toggle.checked = false;
+    } else {
+        checkmark.style.display = "inline";
+        toggle.checked = true;
+    }
+}
+
+function showCheckmark() {
+
+    var checkmark = document.getElementById("checkmark");
+    var toggle = document.getElementById("toggle");
+    if (toggle.checked) {
+        checkmark.style.display = "inline";
+    } else {
+        checkmark.style.display = "none";
+    }
+}
